8eca32a38467e81f53a849560e936ca36489c8df
Adam Shaw <arshaw@users.noreply.github.com>
2021-09-22 12:36:58 -0400

    really include promise stuff

--- promise.d.ts
+++ promise.d.ts
@@ -1,6 +1,7 @@
 /*
 This file is included as a HACK for export maps not working in TypeScript
 https://github.com/microsoft/TypeScript/issues/33079
+After removing this hack, remove the line in package.json::files
 */
 import { Options } from './dist/utils';
 declare function live(command: string | string[], options?: Options): Promise<void>;
--- promise.js
+++ promise.js
@@ -1,4 +1,5 @@
 /*
 See note in promise.d.ts. A JavaScript file is required too.
+After removing this hack, remove the line in package.json::files
 */
 module.exports = require('./dist/promise')
